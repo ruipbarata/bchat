29754bbf6338f619db6c0a6e057061a9761a7ca1
Rui Pedro Barata <ruipbarata@gmail.com>
2021-06-25 00:22:09 +0100

    prepare to start build chat system

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'chat_rooms/index'
+  get 'chat_rooms/new'
+  get 'chat_rooms/create'
   devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :chat_rooms, only: [:new, :create, :show, :index]
+  root 'chat_rooms#index'
+
 end
